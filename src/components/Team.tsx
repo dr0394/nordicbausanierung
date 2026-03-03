690d81a237e93d9f97187b60ecc5326e8873a066
webflixinfo <kontakt@webflix.info>
2026-03-04 00:31:18 +0100

    Updated Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -50,7 +50,7 @@ const Team = () => {
                 <img
                   src={member.image}
                   alt={member.name}
-                  className="w-full h-full object-cover"
+                  className={`w-full h-full object-cover ${member.name === 'Daniela Roth' ? 'scale-75' : ''}`}
                 />
               </div>
               <div className="p-6">
